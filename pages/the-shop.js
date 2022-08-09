794fdf1dc0664a5fa657deb7c50564dc30986986
Alyssa Voccia <avoccia@hotmail.com>
2022-08-08 21:32:36 -0400

    Added the shop page

--- pages/the-shop.js
+++ pages/the-shop.js
@@ -1,8 +1,32 @@
+import Image from 'next/image';
+import theShop from '../static/theShop.jpg';
 import styles from '../styles/TheShop.module.css';
 
 const TheShop = () => {
   return (
-    <section className={styles.theShop}>Check out The Shop!</section>
+    <section className='page-section'>
+      <div className={styles.container}>
+        <div className={styles.titleContainer}>
+          <h1 className="section-title">The Shop</h1>
+        </div>
+        <div className={styles.content}>
+          <div className={styles.imagesContainer}>
+            <Image src={theShop} alt='Different woodworking tools hanging on the wall.' />
+          </div>
+          <div className={styles.favorites}>
+            <div className={styles.favoritesTitleContainer}>
+              <h2 className='section-subtitle'>Tools in the Shop</h2>
+            </div>
+            
+          </div>
+          <div className={styles.recommendations}>
+            <div className={styles.recommendationsTitleContainer}>
+              <h2 className='section-subtitle'>Other Tools I Recommend</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
