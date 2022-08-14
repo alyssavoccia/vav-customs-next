9d71b89c5306aaf88bad02e1dc08cc5ff1ddd185
Alyssa Voccia <avoccia@hotmail.com>
2022-08-14 10:15:20 -0400

    Updated blog post card link

--- components/BlogPostCard.js
+++ components/BlogPostCard.js
@@ -6,7 +6,7 @@ import styles from '../styles/BlogPostCard.module.css';
 const BlogPostCard = ({ blog }) => {
   return (
     <div className={styles.card}>
-      <Link href={`/blog/$${blog.title}`}>
+      <Link href={`/blog/${blog.title.split(' ').join('-')}`}>
         <div className={styles.cardImg}>
           <Image src={`${blog.imgUrl}`} height='200px' width='300px' alt='Blog post cover' />
           {blog.category && <span className={`${styles.imgTag} ${styles[`${blog.category.toLowerCase()}`]}`}>{blog.category}</span>}
@@ -15,7 +15,7 @@ const BlogPostCard = ({ blog }) => {
       <div className={styles.cardBody}>
         <h2>{blog.title}</h2>
         <p>{blog.tagline}</p>
-        <Link href={`/blog/${blog.title}`}><a className={styles.cardBtn}>Read More</a></Link>
+        <Link href={`/blog/${blog.title.split(' ').join('-')}`}><a className={styles.cardBtn}>Read More</a></Link>
       </div>
     </div>
   )
