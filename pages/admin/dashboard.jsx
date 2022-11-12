3b4194de1a99b04e89a8ca18645af1ace47dca27
Alyssa Voccia <avoccia@hotmail.com>
2022-11-12 11:38:25 -0500

    Updated dashboard to be a private route and updated sign in page

--- pages/admin/dashboard.jsx
+++ pages/admin/dashboard.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { auth } from '../../firebase';
 
 const Dashboard = () => {
   return (
     <div>Dashboard</div>
   )
+}
+
+export const getServerSideProps = () => {
+  if (!auth.user) {
+    return {
+      redirect: { destination: '/admin', permanent: false}
+    }
+  }
+
+  return {
+    props: {}
+  }
 }
 
 export default Dashboard;
