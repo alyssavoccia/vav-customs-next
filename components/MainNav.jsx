4c22b5f8901e885fc1e48268a9c85300a898d785
Alyssa Voccia <avoccia@hotmail.com>
2022-11-14 08:14:55 -0500

    Updated nav and footer

--- components/MainNav.jsx
+++ components/MainNav.jsx
@@ -10,6 +10,8 @@ import styles from '../styles/MainNav.module.css';
 const MainNav = () => {
   const { dispatch, totalItemsInCart } = useContext(CartContext);
   const [safari, setSafari] = useState(false);
+  const [active, setActive] = useState(styles.menu);
+  const [mobileIcon, setMobileIcon] = useState(styles.toggler);
 
   useEffect(() => {
     if (global.navigator.userAgent.toLowerCase().includes('safari')) {
@@ -17,43 +19,47 @@ const MainNav = () => {
     }
   }, []);
 
+  const navToggle = () => {
+    if (window.innerWidth < 769) {
+      active === styles.menu ? setActive(`${styles.menu} ${styles.menuActive}`) : setActive(styles.menu)
+    }
+    // Icon toggle
+    mobileIcon === styles.toggler ? setMobileIcon(`${styles.toggler} ${styles.toggle}`) : setMobileIcon(styles.toggler)
+  };
+
   console.log();
   return (
     <nav className={styles.navbar}>
-      <div className={styles.navbarContent}>
-        <div className={styles.logoWrapper}>
-          <Link href='/' passHref><div><Image src={NavLogo} alt='VAV Customs Logo' /></div></Link>
+      <Link href='/' passHref><div><Image className={styles.logo} src={NavLogo} alt='VAV Customs Logo' /></div></Link>
+      <div className={styles.linksCart}>
+        <ul className={active}>
+          <li>
+            <Link href='/#about'>About</Link>
+          </li>
+          <li>
+            <Link href='/#gallery'>Gallery</Link>
+          </li>
+          <li>
+            <Link href='/the-shop'>The Shop</Link>
+          </li>
+          <li>
+            <Link href='/blog'>Blog</Link>
+          </li>
+          <li>
+            <Link href='/custom-build'>Custom Build</Link>
+          </li>
+          <li>
+            <Link href='/store'>Store</Link>
+          </li>
+        </ul>
+        <div className={styles.viewCart} onClick={() => dispatch({ type: 'OPEN_CART' })}>
+          <FontAwesomeIcon className={styles.CartIcon} icon={faCartShopping} />
+          <span className={`${styles.cartCount} ${safari && styles.safariCart}`}>{totalItemsInCart}</span>
         </div>
-        <div>
-          <ul className={styles.menu}>
-            <li>
-              <Link href='/#about'>About</Link>
-            </li>
-            <li>
-              <Link href='/#gallery'>Gallery</Link>
-            </li>
-            <li>
-              <Link href='/the-shop'>The Shop</Link>
-            </li>
-            <li>
-              <Link href='/blog'>Blog</Link>
-            </li>
-            <li>
-              <Link href='/custom-build'>Custom Build</Link>
-            </li>
-            <li>
-              <Link href='/store'>Store</Link>
-            </li>
-            <div className={styles.viewCart} onClick={() => dispatch({ type: 'OPEN_CART' })}>
-              <FontAwesomeIcon className={styles.CartIcon} icon={faCartShopping} />
-              <span className={`${styles.cartCount} ${safari && styles.safariCart}`}>{totalItemsInCart}</span>
-            </div>
-          </ul>
-          <div className={styles.toggler}>
-            <div className={styles.line1}></div>
-            <div className={styles.line2}></div>
-            <div className={styles.line3}></div>
-          </div>
+        <div onClick={navToggle} className={mobileIcon}>
+          <div className={styles.line1}></div>
+          <div className={styles.line2}></div>
+          <div className={styles.line3}></div>
         </div>
       </div>
     </nav>
